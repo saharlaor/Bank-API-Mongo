e3b949fe38bcc775e9e523a201c17c69bbd35345
saharoori <saharlaor@gmail.com>
2022-01-21 18:28:44 +0200

    Setting up empty client with react app

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Imports
 const express = require("express");
+const cors = require("cors");
 const {
   getAllUsers,
   getUser,
@@ -9,14 +10,18 @@ const {
   makeWithdraw,
   makeTransfer,
 } = require("./controllers/users.controllers");
+const path = require("path");
 
 // Constants
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
+const PUBLIC_PATH = path.join(__dirname, "client/build");
 
 // Setup server
 const app = express();
 app.use(express.json());
+app.use(cors());
+app.use(express.static(PUBLIC_PATH));
 
 // Endpoints
 // GET
@@ -32,6 +37,11 @@ app.put("/api/users/deposit/:id", makeDeposit);
 app.put("/api/users/credit/:id", updateUserCredit);
 app.put("/api/users/withdraw/:id", makeWithdraw);
 app.put("/api/users/transfer/", makeTransfer);
+
+// Fallback
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(publicPath, "index.html"));
+});
 
 // Start server
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
