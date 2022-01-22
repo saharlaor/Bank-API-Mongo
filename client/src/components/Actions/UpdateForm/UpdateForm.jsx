2c5e7cc26749b1660428c1edc7c7519ca94caab4
saharoori <saharlaor@gmail.com>
2022-01-22 22:12:34 +0200

    Update page

--- client/src/components/Actions/UpdateForm/UpdateForm.jsx
+++ client/src/components/Actions/UpdateForm/UpdateForm.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./UpdateForm.css";
 
-function UpdateForm() {
-  return <div className="UpdateForm"></div>;
+function UpdateForm({ user: { _id, name, cash, credit } }) {
+  const [mode, setMode] = useState("deposit");
+
+  const handleModeChange = (e) => {
+    setMode(e.target.value);
+  };
+
+  return (
+    <div className="UpdateForm">
+      <h2>{name}</h2>
+      <h4>Cash</h4>
+      <span>{cash}</span>
+      <h4>Credit</h4>
+      <span>{credit}</span>
+      <select name="mode" id="mode" value={mode} onChange={handleModeChange}>
+        <option value="deposit">Deposit</option>
+        <option value="withdraw">Withdraw</option>
+        <option value="credit">Credit</option>
+        <option value="transfer">Transfer</option>
+      </select>
+    </div>
+  );
 }
 
 export default UpdateForm;
